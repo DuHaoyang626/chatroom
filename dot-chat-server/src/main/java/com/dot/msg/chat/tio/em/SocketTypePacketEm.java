cf99c7a0f0f745fda4d9107284e7c21834bf54c8
fusihan <fusihan@noreply.gitcode.com>
2025-06-23 14:10:02 +0800

    20250623

---
+++ dot-chat-server/src/main/java/com/dot/msg/chat/tio/em/SocketTypePacketEm.java
@@ -0,0 +1,31 @@
+package com.dot.msg.chat.tio.em;
+
+import lombok.Getter;
+
+/**
+ * socket消息类型枚举
+ *
+ * @author: fusihan.
+ * @date: Created in 2025/6/18 10:40
+ */
+@Getter
+public enum SocketTypePacketEm {
+
+  /**
+   * 聊天消息
+   */
+  CHAT((byte) 1, "聊天消息"),
+
+  /**
+   * 心跳监测
+   */
+  HEART_BEAT((byte) 2, "心跳监测");
+
+  private final byte type;
+  private final String desc;
+
+  SocketTypePacketEm(byte type, String desc) {
+    this.type = type;
+    this.desc = desc;
+  }
+}
